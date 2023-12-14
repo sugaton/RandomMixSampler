b3c4eb9821aaa5e1698d15b9ce5300c8d7ae10e9
sugaton <s.gohiromu@gmail.com>
2023-12-15 01:23:28 +0900

    Remove unused libraries and refactor

--- sampler.py
+++ sampler.py
@@ -1,9 +1,6 @@
-from typing import Iterator, Optional, Sized
-from torch.utils.data import WeightedRandomSampler, RandomSampler, Sampler, SequentialSampler, SubsetRandomSampler
+from typing import Iterator, Optional
+from torch.utils.data import WeightedRandomSampler, Sampler, SubsetRandomSampler
 import torch
-from copy import copy
-from itertools import chain
-import numpy
 
 class RandomMixSampler(Sampler[int]):
     @staticmethod
@@ -16,18 +13,18 @@ class RandomMixSampler(Sampler[int]):
                  num_samples: Optional[int]= None,
                  generator=None,
                  samplers = list[Sampler[int]]) -> None:
-        self.d = []
+        self.range_func = []
         _total = 0
         _last = 0
         self.samplers = samplers
         for sampler in samplers:
             _total += len(sampler)
-            self.d.append(self.inrange(_last, _total))
+            self.range_func.append(self.inrange(_last, _total))
             _last = _total
-        self.num_samples = _total
-
-    def _get_idx(self):
-        yield from self.idx_gen
+        if num_samples is None:
+            self.num_samples = _total
+        else:
+            self.num_samples = num_samples
 
     def _getter(self, idx):
         for item in self.samplers[idx]:  
@@ -36,7 +33,7 @@ class RandomMixSampler(Sampler[int]):
     def __iter__(self) -> Iterator[int]:
         samplers = [self._getter(i) for i in range(len(self.samplers))]
         for _idx in torch.randperm(self.num_samples).tolist():
-            cand = next(i for i, f in enumerate(self.d) if f(_idx))
+            cand = next(i for i, f in enumerate(self.range_func) if f(_idx))
             yield next(samplers[cand])
 
     def __len__(self):
@@ -47,14 +44,13 @@ if __name__ == "__main__":
     l=[1. if i % 2==0 else 0.1 for i in range(200)] 
     t=torch.tensor(l)
     m = torch.where(t >= 1.)
-    _m = torch.where(t < 1.)
     weights = torch.Tensor(l)
     weights[m] = 0
 
     sampler = WeightedRandomSampler(weights, num_samples=50, replacement=False)
     sampler2 = SubsetRandomSampler(m[0].tolist())
 
-    newsampler = RandomMixSampler(150, samplers=(sampler, sampler2))
+    newsampler = RandomMixSampler(samplers=(sampler, sampler2))
     ids= []
     for idx in newsampler:
         ids.append(idx)
